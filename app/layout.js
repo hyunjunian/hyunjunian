cda362302fd93884c828ba6a1aebd57f1b3821aa
hyunjunian <hyunjunian@gmail.com>
2024-05-09 16:58:49 +0900

    style

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,14 @@ export const metadata = {
 export default function Layout({ children }) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body className="mx-auto max-w-xl">
+        <header className="flex items-center justify-between space-x-4 px-4 py-2">
+          <h1 className="text-2xl sm:text-4xl font-bold italic">Hyunjunian</h1>
+          <p className="text-xs sm:text-base">구독하고 신규 알림을 받아보세요!</p>
+        </header>
+        {children}
+        <footer className="px-4 py-2 text-xs sm:text-base text-center">&copy; {new Date().getFullYear()} 현주니안. 모든 권리 보유</footer>
+      </body>
     </html>
   );
 }
--- app/not-found.js
+++ app/not-found.js
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function NotFound() {
   return (
-    <main>
-      <h1>404 페이지를 찾을 수 없습니다</h1>
+    <main className="flex flex-col justify-center text-center h-96 p-4 space-y-4">
+      <h1 className="text-2xl md:text-4xl">페이지를 찾을 수 없습니다</h1>
+      <p><Link href="/">홈으로</Link></p>
     </main>
   );
 }
