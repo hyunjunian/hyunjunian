ba1a6ab22199056f7b25bf285e72ee3e8319c832
hyunjunian <hyunjunian@gmail.com>
2024-05-17 15:23:13 +0900

    style

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import "./globals.css";
 
 export const metadata = {
@@ -8,13 +9,25 @@ export const metadata = {
 export default function Layout({ children }) {
   return (
     <html lang="ko">
-      <body className="mx-auto max-w-xl">
-        <header className="flex items-center justify-between space-x-4 px-4 py-2">
-          <h1 className="text-2xl sm:text-4xl font-bold italic">Hyunjunian</h1>
-          <p className="text-xs sm:text-base">구독하고 신규 알림을 받아보세요!</p>
+      <body>
+        <header className="fixed top-0 left-0 right-0 h-24 flex items-center justify-between border-b px-6 bg-white">
+          <h1 className="text-2xl md:text-5xl">Hyunjunian</h1>
+          <p className="text-xs md:text-base">구독하고 신규 알림을 받아보세요!</p>
         </header>
-        {children}
-        <footer className="px-4 py-2 text-xs sm:text-base text-center">&copy; {new Date().getFullYear()} 현주니안. 모든 권리 보유</footer>
+        <aside className="fixed top-24 left-0 bottom-0 p-6 w-96 border-r bg-white space-y-6">
+          <nav>
+            <ul className="space-y-3">
+              <li><Link href="/">전체</Link></li>
+              {["소프트웨어", "투자", "게임", "영상"].map((category) => (
+                <li key={category}><Link href={`/?category=${category}`}>{category}</Link></li>
+              ))}
+            </ul>
+          </nav>
+          <footer>&copy; {new Date().getFullYear()} 현주니안. 모든 권리 보유</footer>
+        </aside>
+        <main className="mt-24 ml-96">
+          {children}
+        </main>
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -3,32 +3,24 @@ import Link from "next/link";
 export default function Page() {
   return (
     <>
-      <nav className="sticky top-0 flex flex-nowrap items-center space-x-4 overflow-x-auto px-4 py-2 bg-white border-b">
-        <Link className={`shrink-0 font-bold`} href="/">전체</Link>
-        {["소프트웨어", "투자", "게임", "영상"].map((category) => (
-          <Link className={`shrink-0`} key={category} href={`/?category=${category}`}>{category}</Link>
+      <ul>
+        {[0, 1, 2, 3, 4, 5, 6].map((_, index) => (
+          <li key={index}>
+            <article className="flex flex-col space-y-6 p-12">
+              <header className="flex items-center space-x-2 font-medium text-4xl">
+                <Link href="#소프트웨어">2024년 5월 8일</Link>
+                <Link className="text-neutral-500" href="#소프트웨어">#소프트웨어</Link>
+                <Link className="text-neutral-500" href="#소프트웨어">#투자</Link></header>
+              <Link href="#소프트웨어">
+                <p>나의 투자 원칙 내가 소유할 주식은 오직 아래 2가지다.<br /><br />1. 가치를 매길 수 없는 주식(priceless stocks)<br />2. 싼 주식(cheap stocks)..</p>
+              </Link>
+              <footer>
+                <Link className="text-neutral-500 font-bold" href="#소프트웨어">더 보기</Link>
+              </footer>
+            </article>
+          </li>
         ))}
-      </nav>
-      <main>
-        <ul>
-          {[0, 1, 2, 3, 4, 5, 6].map((_, index) => (
-            <li key={index}>
-              <article className="flex flex-col space-y-2 p-4 border-b">
-                <header className="flex items-center space-x-2 font-bold">
-                  <Link href="#소프트웨어">2024년 5월 8일</Link>
-                  <Link className="text-neutral-500" href="#소프트웨어">#소프트웨어</Link>
-                  <Link className="text-neutral-500" href="#소프트웨어">#투자</Link></header>
-                <Link href="#소프트웨어">
-                  <p>나의 투자 원칙 내가 소유할 주식은 오직 아래 2가지다.<br /><br />1. 가치를 매길 수 없는 주식(priceless stocks)<br />2. 싼 주식(cheap stocks)..</p>
-                </Link>
-                <footer>
-                  <Link className="text-neutral-500 font-bold" href="#소프트웨어">더 보기</Link>
-                </footer>
-              </article>
-            </li>
-          ))}
-        </ul>
-      </main>
+      </ul>
     </>
   );
 }
